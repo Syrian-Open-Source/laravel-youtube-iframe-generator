ba975434b073892179b4dc5267a3c2dea97c9c78
karam  mustafa <karam2mustafa@gmail.com>
2021-12-29 15:16:23 +0200

    wip

--- src/Abstracts/YFrameAbstract.php
+++ src/Abstracts/YFrameAbstract.php
@@ -4,4 +4,125 @@ namespace SOS\LaravelYoutubeFrameGenerator\Classes;
 
 class YFrameAbstract
 {
+    private $css = '';
+    private $id = '';
+    private $fullscreen = true;
+    private $attributes = '';
+
+    /**
+     * @return mixed
+     * @author karam mustaf
+     */
+    public function getCss()
+    {
+        return $this->css;
+    }
+
+    /**
+     * @param  mixed  $css
+     *
+     * @return YFrameAbstract
+     * @author karam mustaf
+     */
+    public function setCss($css)
+    {
+        $this->css .= $css;
+
+        return $this;
+    }
+
+    /**
+     * @param $url
+     *
+     * @return mixed
+     * @author karam mustaf
+     */
+    public function getId($url)
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param  mixed  $id
+     *
+     * @return YFrameAbstract
+     * @author karam mustaf
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     * @author karam mustaf
+     */
+    public function getFullscreen()
+    {
+        return $this->fullscreen;
+    }
+
+    /**
+     * @param  string  $fullscreen
+     *
+     * @return YFrameAbstract
+     * @author karam mustaf
+     */
+    public function setFullscreen($fullscreen)
+    {
+        $this->fullscreen = $fullscreen;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     * @author karam mustaf
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @param  string  $attributes
+     *
+     * @return YFrameAbstract
+     * @author karam mustaf
+     */
+    public function setAttributes($attributes)
+    {
+        $this->attributes .= $attributes;
+
+        return $this;
+    }
+
+    /**
+     * set the default width.
+     *
+     * @param  int  $width
+     * @param  string  $unit
+     *
+     * @author karam mustafa
+     */
+    public function width($width = 500, $unit = "px")
+    {
+        $this->setCss("width:".$width.$unit.';');
+    }
+
+    /**
+     * set the default height.
+     *
+     * @param  int  $height
+     * @param  string  $unit
+     *
+     * @author karam mustafa
+     */
+    public function height($height = 300, $unit = "px")
+    {
+        $this->setCss("height:".$height.$unit.';');
+    }
+
 }
--- src/Classes/YFrame.php
+++ src/Classes/YFrame.php
@@ -15,10 +15,47 @@ class YFrame extends YFrameAbstract implements YFrameInterface
 {
 
     /**
+     * @param  string  $url
+     *
+     * @return mixed
+     * @author karam mustaf
+     */
+    public function getId($url)
+    {
+        // check if the youtube link has the following formant:
+        // youtube.com/watch?v=xxxxx or youtube.com/?v=xxxxx
+        $res = strpos($url, '?v=');
+        if ($res !== false) {
+            $t = explode("?v=", $url);
+            return $t[count($t) - 1];
+        }
+
+        // check if the youtube link has the following formant:
+        // youtu.be.com/xxxx
+        $res = strpos($url, 'youtu.be');
+        if ($res !== false) {
+            $t = explode($url, '/');
+            return $t[count($t) - 1];
+        }
+
+        //otherwise assume that the user passed the id
+        return $url;
+    }
+
+
+    /**
      * @inheritDoc
      */
     public function generate($url)
     {
-        // TODO: Implement generate() method.
+        return sprintf(
+            '<iframe src="https://www.youtube.com/embed/%s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" %s %s style="%s"></iframe>',
+            $this->getId($url),
+            $this->getFullscreen() ? 'allowfullscreen' : '',
+            $this->getAttributes(),
+            $this->getCss()
+        );
     }
+
+
 }
